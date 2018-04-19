00759a5edfb15fe619ac2e897cc15bb1fe6da685
Kristen M SPECKHALS <kristenmspeckhals@Kristens-MacBook-Pro.local>
2018-04-19 12:35:42 -0400

    changes to modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,15 +4,15 @@ var friendArray = [
   photo :"http://www.procopytips.com/photos/gilligan.jpg",
   scores :[
       5,
-      1,
-      4,
-      4,
       5,
-      1,
-      2,
       5,
-      4,
-      1
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5
     ]
 },
 {
@@ -20,63 +20,63 @@ var friendArray = [
     photo: "https://cbsnews3.cbsistatic.com/hub/i/r/2014/09/26/0a3dac5e-bdb1-4dc1-a6f3-3f50d2722f25/resize/620x465/d8d8af9972a3eb92e4446e9f074f93ec/alanhale.jpg",
     scores: [
         4,
-        2,
-        3,
         4,
-        1,
-        1,
-        2,
-        3,
-        2,
-        1
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4
       ]
   },
   {
     name: "Professor",
     photo :"https://i.pinimg.com/originals/82/73/e6/8273e600e9dce2ba5e0179f2552665ea.jpg",
     scores :[
-        1,
-        1,
-        2,
-        4,
-        2,
-        1,
-        2,
-        2,
-        4,
-        5
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3
       ]
   },
   {
     name: "Ginger",
     photo: "http://images2.fanpop.com/images/photos/3700000/Gilligan-s-Island-Ginger-classic-television-revisited-3726668-420-500.jpg",
     scores :[
-        5,
-        1,
-        4,
-        3,
         2,
-        3,
         2,
-        5,
-        4,
-        1
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2
       ]
   },
   {
     name: "Mary Ann",
     photo: "http://images2.fanpop.com/images/photos/3700000/Gilligan-s-Island-Mary-Ann-classic-television-revisited-3727368-438-329.jpg",
     scores: [ 
-        3,
-        5,
-        5,
         1,
         1,
         1,
         1,
         1,
-        2,
-        5
+        1,
+        1,
+        1,
+        1,
+        1
       ]
   }
 ]
